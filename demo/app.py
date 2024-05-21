582ff0da70528543c9eaaf876fdfb952dda82129
freddyaboulton <alfonsoboulton@gmail.com>
2024-05-21 16:42:26 -0400

    format

--- demo/app.py
+++ demo/app.py
@@ -40,13 +40,16 @@ def interact_with_agent(prompt, messages):
 async def interact_with_langchain_agent(prompt, messages):
     messages.append(ChatMessage(role="user", content=prompt))
     yield messages
-    async for chunk in langchain_agent.astream(
-        {"input": prompt}
-    ):
+    async for chunk in langchain_agent.astream({"input": prompt}):
         if "steps" in chunk:
             for step in chunk["steps"]:
-                messages.append(ChatMessage(role="assistant", content=step.action.log,
-                                  thought_metadata={"tool_name": step.action.tool}))
+                messages.append(
+                    ChatMessage(
+                        role="assistant",
+                        content=step.action.log,
+                        thought_metadata={"tool_name": step.action.tool},
+                    )
+                )
                 yield messages
         if "output" in chunk:
             messages.append(ChatMessage(role="assistant", content=chunk["output"]))
@@ -76,7 +79,9 @@ with gr.Blocks() as demo:
                 ],
             )
             input_2 = gr.Textbox(lines=1, label="Chat Message")
-            input_2.submit(interact_with_langchain_agent, [input_2, chatbot_2], [chatbot_2])
+            input_2.submit(
+                interact_with_langchain_agent, [input_2, chatbot_2], [chatbot_2]
+            )
         with gr.Tab("Docs"):
             gr.Markdown(Path(current_dir / "docs.md").read_text())
 
